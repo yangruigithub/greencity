42d0485e22778375364187ad8dc42350374964c3
yangruigithub <727545512@qq.com>
2016-09-25 20:23:34 +0800

    correction v1.2

--- js/swipper.js
+++ js/swipper.js
@@ -1,6 +1,65 @@
+//  ========== 
+//  = 导航点 = 
+//  ========== 
+
 var aDots=document.getElementsByClassName('dots');
 
+for(i=0;i<aDots.length;i++)
+{
+	(function (i){
+				aDots[i].onclick=function () {
+					for(i=0;i<aDots.length;i++) {
+						aDots[i].style.background='none';
+						aDots[i].style.width='15px';
+					}
+					this.style.background='#fff';
+					this.style.width='35px';
+				};
+	})(i);				
+}
 
+//  ========== 
+//  = swipper = 
+//  ==========
+
+
+
+
+
+
+//  ========== 
+//  = 伪自适应 = 
+//  ========== 
+window.onresize=function ()　{
+	var container=document.getElementById('container_wrapper');
+	var bg=document.getElementById('bg_wrapper');
+	var swipper=document.getElementById('swipper_wrapper');
+	var img=document.getElementsByTagName('img')[1];
+	swipper.style.height=img.offsetHeight+'px';
+	container.style.height=document.getElementById('swipper_wrapper').offsetHeight+'px';
+	bg.style.height=document.getElementById('swipper_wrapper').offsetHeight+'px';//不要忘记单位
+};
+window.onload=function ()　{
+	var container=document.getElementById('container_wrapper');
+	var bg=document.getElementById('bg_wrapper');
+	var swipper=document.getElementById('swipper_wrapper');
+	var img=document.getElementsByTagName('img')[1];
+	swipper.style.height=img.offsetHeight+'px';
+	container.style.height=document.getElementById('swipper_wrapper').offsetHeight+'px';
+	bg.style.height=document.getElementById('swipper_wrapper').offsetHeight+'px';//不要忘记单位
+	timer_auto_swipper=setInterval(function (){
+		swipper.style.top=Math.parseInt(swipper.style.top)-Math.parseInt(img.style.top)+'px';
+		document.title=swipper.style.top;
+		
+	},500);
+};
+
+
+
+
+
+/*
+stupid
 
 for(i=0;i<aDots.length;i++)
 {
@@ -98,3 +157,4 @@ function fnDots (i){
 	}
 
 }
+*/
